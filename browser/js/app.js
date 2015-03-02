5ffe661fc401bcd7ec47ef1f519a61084ce3cc6d
Heather Pike <heatherlaurenpike@gmail.com>
2015-03-02 17:13:06 -0500

    HP and RM updated states and created folders for cart, login, signup, checkout and started on log_in routes and form

--- browser/js/app.js
+++ browser/js/app.js
@@ -6,8 +6,10 @@ app.controller('MainController', function ($scope) {
     // Given to the <navbar> directive to show the menu.
     $scope.menuItems = [
         { label: 'Home', state: 'home' },
-        { label: 'About', state: 'about' },
-        { label: 'Tutorial', state: 'tutorial' }
+        { label: 'My Cart', state: 'my_cart' },
+        { label: 'Log In', state: 'log_in' },
+        { label: 'Sign Up', state: 'sign_up'},
+        { label: 'Checkout'}, state: 'checkout'}
     ];
 
 });
