1eb6bc466c13acd70c15bf036ed2f51d920da639
Richard Moore <Rmoore424@gmail.com>
2015-03-03 14:57:43 -0500

    added widget directive for products and route to get all products

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -9,5 +9,18 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('HomeCtrl', function ($scope) {
+app.controller('HomeCtrl', function ($scope, HomeFactory) {
+	HomeFactory.getProducts().then(function (products) {
+		$scope.products = products;
+	});
+});
+
+app.factory('HomeFactory', function ($http) {
+	return {
+		getProducts: function () {
+			return $http.get('/api/products/products').then(function (response) {
+				return response.data;
+			});
+		}
+	};
 });
