90221e1514c3275c17d33c92690b305ea2025c29
Heather Pike <heatherlaurenpike@gmail.com>
2015-03-03 18:34:08 -0500

    Heather Pike - started work on admin states

--- browser/js/admin/admin.js
+++ browser/js/admin/admin.js
@@ -10,21 +10,29 @@ app.config(function ($stateProvider) {
 		controller: 'AdminCtrl',
 		templateUrl: 'js/admin/admin.html'
 	});
+
+	$stateProvider.state('admin.editUser', {
+		url: '/:user/editUser',
+		controller: 'AdminCtrl',
+		templateUrl: 'js/admin/editUser.html'
+	});
 });
 
-app.controller('AdminCtrl', function($scope, $state, AdminFactory) {
+app.controller('AdminCtrl', function($scope, $state, $stateParams, AdminFactory) {
 	$scope.editUser = function (email) {
-		adminFactory.getUser(email).then(function (user) {
-			$state.go('editUser')
-		})
-	}
-})
+		AdminFactory.getUser(email).then(function (user) {
+			var user = $stateParams.user.email;
+			//how do we make the state below pull in the $scope.user data? /:user
+			$state.go('admin.editUser', { user: user});
+		});
+	};
+});
 
 app.factory('AdminFactory', function($http) {
 	return {
 		getUser: function(email) {
-			$http.get('/admin', { params: { email: email }}
-			});
+			$http.get('/admin', { params: { email: email } }
+			);
 		}
 	};
 });
