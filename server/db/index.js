1777b7a9c1b27f0c89303048b31b7508994f439f
Eric Gonzalo <ersgonzalo@gmail.com>
2015-03-04 10:58:16 -0500

    Update models for categories and files

--- server/db/index.js
+++ server/db/index.js
@@ -11,10 +11,12 @@ var db = mongoose.connect(DATABASE_URI).connection;
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
+require('./models/category');
 require('./models/order');
 require('./models/vacation');
 require('./models/review');
 require('./models/user');
+
 
 var startDbPromise = new Q(function (resolve, reject) {
     db.on('open', resolve);
