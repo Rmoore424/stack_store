36882e53f57b630eba8d6299c9432190b92447f3
Alice Kindheart <moonstonecowgirl@gmail.com>
2015-03-03 20:41:01 -0500

    added 4 vacations

---
+++ server/db/vacations.js
@@ -0,0 +1,33 @@
+var vacations = [ 
+	{name: "The Holodeck",
+	description: "Too many places you'd like to visit? Why not visit them all with only one vacation?"
+	price: 5000,
+	category: "Otherworldy",
+	imageUrl: "http://www.thescrib.com/wp-content/uploads/2013/04/holodeck.jpg"
+	},
+	{name: 'Biking in the French Alps',
+	description: 'Cycle the most famous climbs in the Tour de France, on our 9-day adventure which spans the French Pyrenees, Mont Ventoux and the Alps.'
+	price: 5000, 
+	category: 'Adventure',
+	imageUrl: 'http://trektravel.com/wp-content/uploads/2013/08/classic-climbs-tour-04-1600x670-150x150.jpg'
+	},
+	{name: 'Rafting down the Colorado River',
+	description: 'Spend a full day white water rafting down the beautiful Colorado River. Complete with a hot picnic lunch!',
+	price: 1000,
+	category: 'Adventure',
+	imageUrl: 'http://www.coloradowhitewaterrafting.com/img/hero/three.jpg'
+	},
+	{name: 'Discover Patagonia',
+	description: 'Bring your backpack and your hiking boots for this 49 day trip starting from Chile’s modern metropolis into the beautiful lake districts of Chile and Argentina before passing into remote and windy Patagonia to the world’s largest glacier and most southerly town.',
+	price: 10000,
+	category: 'Adventure',
+	imageUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcToclLkyRNYgHnwhGqIzDW8mJ1iJCIEbdlaCfkAah1o0OCAu7Mn8g'
+	},
+	{name: ''
+
+	}
+
+
+
+
+];
