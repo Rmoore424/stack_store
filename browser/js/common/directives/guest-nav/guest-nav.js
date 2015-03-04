27c4a66e97da68bf70705ffef39d0436e7b6b82a
Richard Moore <Rmoore424@gmail.com>
2015-03-04 12:07:40 -0500

    added directives for nav lists

---
+++ browser/js/common/directives/guest-nav/guest-nav.js
@@ -0,0 +1,7 @@
+'use strict';
+app.directive('guestnav', function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/common/directives/guest-nav/guest-nav.html'
+	};
+});
---
+++ browser/js/common/directives/user-nav/user-nav.js
@@ -0,0 +1,7 @@
+'use strict';
+app.directive('usernav', function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/common/directives/user-nav/user-nav.html'
+	};
+});
