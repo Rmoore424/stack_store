bcc423f1fc7e0c2c23d67591fd5938a3651af660
Richard Moore <Rmoore424@gmail.com>
2015-03-06 15:14:40 -0500

    fixed typo

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
     name: {type: String, required: true}
-    // description: String
+    description: String
 });
 
 mongoose.model('Category', schema);
