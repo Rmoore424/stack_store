f76e93c424ab25e97081c672b910497975e96368
Alice Kindheart <moonstonecowgirl@gmail.com>
2015-03-06 15:10:44 -0500

    HP and AK have successfully seeded the databasegit add -A

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -2,8 +2,8 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    name: {type: String, required: true},
-    description: String
+    name: {type: String, required: true}
+    // description: String
 });
 
 mongoose.model('Category', schema);
