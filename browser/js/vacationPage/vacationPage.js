79567a17f4e9357583ddbb0bcf26d470288e2089
Alice Kindheart <moonstonecowgirl@gmail.com>
2015-03-09 13:09:11 -0400

    Can now add a product to the cart from the product page

--- browser/js/vacationPage/vacationPage.js
+++ browser/js/vacationPage/vacationPage.js
@@ -9,6 +9,14 @@ app.config(function ($stateProvider) {
 	});
 });
 
+// app.config(function ($stateProvider) {
+// 	$stateProvider.state('cart', {
+// 		url: '/cart',
+// 		controller: 'HomeCtrl',
+// 		templateUrl: 'js/cart/cart.html'
+// 	});
+// });
+
 app.controller('VacationPgCtrl', function($scope, $stateParams, $state, VacationsFactory, ReviewFactory, CartFactory) {
 	VacationsFactory.getOneVacation($stateParams.id).then(function(vacation) {
 		$scope.vacation = vacation;
@@ -16,8 +24,11 @@ app.controller('VacationPgCtrl', function($scope, $stateParams, $state, Vacation
 
 	$scope.addToCart = function(product) {
 		CartFactory.getCart().then(function(cart) {
+			console.log('cart', cart);
 			cart.items.push({product: product._id, quantity: 1});	
 		});
+		$state.go('cart');
+
 	};
 
 	var setUpReviews = function (){
